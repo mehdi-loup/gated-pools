78edc304b715fa0bfc18d54288787a8ed8989fa6
mehdi-loup <mehdiloup.nasom@gmail.com>
2025-05-31 19:56:31 +0200

    add proof provider

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -8,6 +8,8 @@ import { injected, metaMask, safe } from 'wagmi/connectors'
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
 import { useEffect, useState } from 'react'
 import { BlockieAvatar } from '@/components/BlockieAvatar'
+import { ProofProvider } from '@vlayer/react'
+import { proverConfig } from '@/hooks/vlayer/helpers'
 
 const config = createConfig({
   chains: [mainnet, base, sepolia],
@@ -38,7 +40,9 @@ export function Providers({ children }: { children: React.ReactNode }) {
         <RainbowKitProvider
           avatar={BlockieAvatar}
         >
-          {children}
+          <ProofProvider config={proverConfig}>
+            {children}
+          </ProofProvider>
         </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
--- frontend/hooks/vlayer/helpers.ts
+++ frontend/hooks/vlayer/helpers.ts
@@ -17,3 +17,9 @@ export const EMAIL_VERIFIER_ADDRESS =
 export const DNS_SERVICE_URL = process.env.NEXT_PUBLIC_DNS_SERVICE_URL!;
 export const VLAYER_API_TOKEN = process.env.NEXT_PUBLIC_VLAYER_API_TOKEN!;
 export const PROVER_GAS_LIMIT = process.env.NEXT_PUBLIC_PROVER_GAS_LIMIT!;
+export const PROVER_URL=process.env.NEXT_PUBLIC_PROVER_URL!;
+
+export const proverConfig = {
+  proverUrl: PROVER_URL,
+  token: VLAYER_API_TOKEN,
+};
