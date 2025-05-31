d1f428ed4c14a1dbeab8f4786c7116c288e1bcce
Raouf <raouf.hec@gmail.com>
2025-05-31 15:43:40 +0200

    feat: vlayer hook logic half done

--- frontend/hooks/vlayer/helpers.ts
+++ frontend/hooks/vlayer/helpers.ts
@@ -12,3 +12,6 @@ export const getTargetEmailAddressAndId = (): {
 export const EMAIL_SERVICE_URL = "";
 export const EMAIL_PROVER_ADDRESS = "";
 export const EMAIL_VERIFIER_ADDRESS = "";
+export const DNS_SERVICE_URL = "";
+export const VLAYER_API_TOKEN = "";
+export const PROVER_GAS_LIMIT = 2_000_000;
--- frontend/hooks/vlayer/useProveEmail.ts
+++ frontend/hooks/vlayer/useProveEmail.ts
@@ -1,144 +1,84 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import {
   useWriteContract,
   useWaitForTransactionReceipt,
   useAccount,
+  useChainId,
 } from "wagmi";
 import {
   useCallProver,
   useWaitForProvingResult,
   useChain,
 } from "@vlayer/react";
-import { preverifyEmail } from "@vlayer/sdk";
+import { BrandedHash, preverifyEmail } from "@vlayer/sdk";
 import emailProverSpec from "./abis/EmailDomainDAOProver.json";
 import emailVerifierSpec from "./abis/EmailDomainDAOVerifier.json";
-import { AbiStateMutability, ContractFunctionArgs } from "viem";
+import { Abi, AbiStateMutability, Address, ContractFunctionArgs } from "viem";
+import {
+  DNS_SERVICE_URL,
+  EMAIL_PROVER_ADDRESS,
+  PROVER_GAS_LIMIT,
+  VLAYER_API_TOKEN,
+} from "./helpers";
 
-export const
+type useGetEmailProofOutput = {
+  startProving: () => Promise<void>;
+  proofHash: BrandedHash<Abi, string> | null;
+  callProverError: Error | null;
+};
 
-export const useProveEmail = () => {
-  const { address } = useAccount();
-
-  const {
-    writeContract,
-    data: txHash,
-    error: verificationError,
-    status,
-  } = useWriteContract();
-
-  const { status: onChainVerificationStatus } = useWaitForTransactionReceipt({
-    hash: txHash,
-  });
-
-  const { chain, error: chainError } = useChain(
-    import.meta.env.VITE_CHAIN_NAME,
-  );
-  if (chainError) {
-    throw new UseChainError(chainError);
-  }
-
+const useGetEmailProof = (
+  emlContent: string,
+  expectedDomain: string,
+): useGetEmailProofOutput => {
+  const chainId = useChainId();
   const {
     callProver,
     data: proofHash,
     error: callProverError,
   } = useCallProver({
-    address: import.meta.env.VITE_PROVER_ADDRESS,
-    proverAbi: proverSpec.abi,
+    address: EMAIL_PROVER_ADDRESS as Address,
+    proverAbi: emailProverSpec.abi as any,
     functionName: "main",
-    gasLimit: Number(import.meta.env.VITE_GAS_LIMIT),
-    chainId: chain?.id,
+    gasLimit: PROVER_GAS_LIMIT,
+    chainId: chainId,
   });
 
-  if (callProverError) {
-    throw new CallProverError(callProverError.message);
-  }
-
-  const { data: proof, error: provingError } =
-    useWaitForProvingResult(proofHash);
-
-  if (provingError) {
-    throw new CallProverError(provingError.message);
-  }
-
-  const verifyProofOnChain = async () => {
-    setCurrentStep(ProofVerificationStep.VERIFYING_ON_CHAIN);
-
-    if (!proof) {
-      throw new NoProofError("No proof available to verify on-chain");
-    }
-
-    const contractArgs: Parameters<typeof writeContract>[0] = {
-      address: import.meta.env.VITE_VERIFIER_ADDRESS,
-      abi: verifierSpec.abi,
-      functionName: "verify",
-      args: proof as unknown as ContractFunctionArgs<
-        typeof verifierSpec.abi,
-        AbiStateMutability,
-        "verify"
-      >,
-    };
-
-    await ensureBalance(address as `0x${string}`, balance?.value ?? 0n);
-
-    writeContract(contractArgs);
-  };
-
-  const [preverifyError, setPreverifyError] = useState<Error | null>(null);
-  const startProving = async (emlContent: string) => {
-    setCurrentStep(ProofVerificationStep.SENDING_TO_PROVER);
-
+  const startProving = useCallback(async () => {
     try {
       const email = await preverifyEmail({
         mimeEmail: emlContent,
-        dnsResolverUrl: import.meta.env.VITE_DNS_SERVICE_URL,
-        token: import.meta.env.VITE_VLAYER_API_TOKEN,
+        dnsResolverUrl: DNS_SERVICE_URL,
+        token: VLAYER_API_TOKEN,
       });
-      await callProver([email]);
+      await callProver([email, expectedDomain]);
     } catch (error) {
-      setPreverifyError(error as Error);
+      console.log(error);
     }
-    setCurrentStep(ProofVerificationStep.WAITING_FOR_PROOF);
-  };
+  }, [emlContent, expectedDomain, callProver]);
+  return { startProving, proofHash, callProverError };
+};
 
-  useEffect(() => {
-    if (proof) {
-      log("proof", proof);
-      void verifyProofOnChain();
-    }
-  }, [proof]);
-
-  useEffect(() => {
-    if (status === "success" && proof) {
-      setCurrentStep(ProofVerificationStep.DONE);
-      const proofArray = proof as unknown[];
-      void navigate(
-        `/success?txHash=${txHash}&domain=${String(proofArray[3])}&recipient=${String(proofArray[2])}`,
-      );
-    }
-  }, [status]);
-
-  useEffect(() => {
-    if (verificationError) {
-      if (verificationError.message.includes("already been minted")) {
-        throw new AlreadyMintedError();
-      }
-      throw new Error(verificationError.message);
-    }
-  }, [verificationError]);
-
-  useEffect(() => {
-    if (preverifyError) {
-      throw new PreverifyError(preverifyError.message);
-    }
-  }, [preverifyError]);
+type useProveEmailOutput = {
+  startProving: () => Promise<void>;
+  proof: any;
+  error: Error | null;
+};
+export const useProveEmail = (
+  emlContent: string,
+  expectedDomain: string,
+): useProveEmailOutput => {
+  const { startProving, proofHash, callProverError } = useGetEmailProof(
+    emlContent,
+    expectedDomain,
+  );
+  const { data: proof, error: provingError } =
+    useWaitForProvingResult(proofHash);
 
   return {
-    currentStep,
-    txHash,
-    onChainVerificationStatus,
-    verificationError,
-    provingError,
     startProving,
+    proof,
+    error: provingError || callProverError,
   };
 };
 
